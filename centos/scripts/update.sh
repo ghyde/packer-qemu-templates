404b4131481903637fe6865fe00b23a3541e2541
Clint Savage <herlo1@gmail.com>
2015-11-30 12:01:41 -0700

    adding a simple salt minion config to enable minion-only management

--- centos/scripts/update.sh
+++ centos/scripts/update.sh
@@ -1,3 +1,23 @@
 yum -y update
 yum -y upgrade
 yum -y install salt-minion
+cat << EOF > /etc/salt/minion
+file_client: local
+
+file_roots:
+  base:
+    - states
+
+pillar_roots:
+  base:
+    - pillar
+
+root_dir: .
+
+disable_modules:
+    - ipmi
+    - nacl
+    - npm
+    - tls
+    - zfs
+EOF
