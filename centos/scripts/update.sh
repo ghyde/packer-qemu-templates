f2664acc06c4848859d6c9ffca25b9a9fcb3efc8
Clint Savage <herlo1@gmail.com>
2015-12-14 12:34:49 -0700

    add python-nose and nfs-utils

--- centos/scripts/update.sh
+++ centos/scripts/update.sh
@@ -1,23 +1,4 @@
 yum -y update
 yum -y upgrade
 yum -y install salt-minion
-cat << EOF > /etc/salt/minion
-file_client: local
-
-file_roots:
-  base:
-    - states
-
-pillar_roots:
-  base:
-    - pillar
-
-root_dir: .
-
-disable_modules:
-    - ipmi
-    - nacl
-    - npm
-    - tls
-    - zfs
-EOF
+yum -y install python-nose nfs-utils
